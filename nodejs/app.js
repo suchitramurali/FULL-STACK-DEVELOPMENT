d657f02d6532f43cbc618b838d8c68664d86cb85
root <suchitramurali737@gmail.com>
2019-04-08 16:22:21 +0530

    json

--- nodejs/app.js
+++ nodejs/app.js
@@ -4,9 +4,12 @@ app.get('/',(req,res)=>{
     res.send("how are you");
 });
 app.get('/home',(req,res)=>{
-    res.send("welcome")
+    res.send("<h1>welcome</h1>")
 });
 app.get('/contact',(req,res)=>{
-    res.send("contact us");
+    res.send('[{"name":"jerry","age:32},{"name":"tom","age":32}]');
 });
+
+
 app.listen(3000);
+
