628a2df298a2a125d7c500aad21f25e074d1a962
root <suchitramurali737@gmail.com>
2019-04-10 16:20:06 +0530

    te

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,12 +1,24 @@
 const express=require('express');
+const exbs=require('express-handlebars')
 var app=express();
+
+app.engine('handlebars',exbs());
+app.set('view engine','handlebars');
+
+
+
+
+
+
 app.get('/',(req,res)=>{
     res.send("how are you");
 });
+
 app.get('/home',(req,res)=>{
     res.send("<h1>welcome</h1>")
 });
-app.get('/contact',(req,res)=>{
+
+app.get('/tab',(req,res)=>{
     res.send('[{"name":"jerry","age:32},{"name":"tom","age":32}]');
 });
 
